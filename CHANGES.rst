348631b7c0f247781ad41b8c3faf474eed1f398a
Chris Meyer <cmeyer1969@gmail.com>
2020-05-01 18:49:00 -0700

    Switch to declarative setup.cfg + pyproject file. Require Python 3.7.

---
+++ CHANGES.rst
@@ -0,0 +1,6 @@
+Changelog (nionswift-matplotlib)
+================================
+
+0.1.0 (INITIAL VERSION)
+-----------------------
+- Merely a proof of concept.
--- setup.py
+++ setup.py
@@ -1,30 +1,4 @@
-# -*- coding: utf-8 -*-
+from setuptools import setup
 
-"""
-To upload to PyPI, PyPI test, or a local server:
-python setup.py bdist_wheel upload -r <server_identifier>
-"""
-
-import setuptools
-import os
-
-setuptools.setup(
-    name="nionswift-matplotlib",
-    version="0.1.0",
-    author="Nion Software",
-    author_email="swift@nion.com",
-    description="Matplotlib backend for using matplotlib directly within Nion Swift.",
-    long_description=open("README.rst").read(),
-    url="https://github.com/nion-software/nionswift-matplotlib",
-    packages=["nion.matplotlib"],
-    install_requires=['matplotlib'],
-    license='Apache 2.0',
-    classifiers=[
-        "Development Status :: 2 - Pre-Alpha",
-        "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.5",
-    ],
-    include_package_data=True,
-    test_suite="nion.matplotlib.test",
-    python_requires='~=3.5',
-)
+if __name__ == "__main__":
+    setup()
